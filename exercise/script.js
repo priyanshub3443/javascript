6373975bf97ff2d0a34c81c2484e870481ecf50d
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-08-28 15:22:39 +0530

    complet exersize

--- exercise/script.js
+++ exercise/script.js
@@ -1,3 +1,41 @@
-let date= new Date();
-date.getHours();
-console.log(date);
+let noogTimeClicked = localStorage.getItem('noogTimeClicked' || 0);
+
+function buttonpressed() {
+  noogTimeClicked++;
+  localStorage.setItem('noogTimeClicked', noogTimeClicked);
+  updateButton();
+}
+
+function updateButton(){
+  let button = document.querySelector('#my-button');
+
+  if (noogTimeClicked % 2 == 0) {
+    button.classList.remove('odd');
+    button.classList.add('even');
+  } else {
+    button.classList.remove('even');
+    button.classList.add('odd');
+  }
+  button.innerText = noogTimeClicked;
+}
+updateButton();
+
+
+
+
+
+
+function doGreeting(personName = 'dear') {
+  let date = new Date();
+  let hours = date.getHours();
+  let heading = document.querySelector('#greeting');
+  if (hours > 5 && hours < 12) {
+    heading.innerText = `Good Moring ${personName}`;
+  } else if (hours > 12 && hours < 18) {
+    heading.innerText = `Good Aftone ${personName}`;
+  } else {
+    heading.innerText = `Good Night ${personName}`
+  }
+}
+
+doGreeting();
