427d3155ad228a176321d81ea7d2e80ba7b59a8d
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-11-01 00:37:55 +0530

    js project complete

--- myntra project/scripts/index.js
+++ myntra project/scripts/index.js
@@ -1,8 +1,40 @@
-let itemsContainerElement = document.querySelector('.items-container');
+let bagItems;
 
-let innerHTML='';
-items.forEach(item=>{
-  innerHTML += `
+onLoad();
+function onLoad(){
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems= bagItemsStr ? h=JSON.parse(bagItemsStr):[];
+displayItemsOnHomePage();
+displayBagIcon();
+}
+
+
+function addToBag(itemId) {
+    bagItems.push(itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    displayBagIcon();
+}
+
+function displayBagIcon() {
+    
+    let bagItemCountElement = document.querySelector( '.bag-items-count');
+    if(bagItems.length>0){
+        bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+    }else{
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+}
+
+function displayItemsOnHomePage() {
+    let itemsContainerElement = document.querySelector('.items-container');
+    if(!itemsContainerElement){
+        return;
+    }
+
+    let innerHTML = '';
+    items.forEach(item => {
+        innerHTML += `
   <div class="item-container">
                 <img class="item-image" src="${item.image}" alt="item image">
                 <div class="rating">
@@ -15,8 +47,16 @@ items.forEach(item=>{
                     <span class="original-price">Rs ${item.original_price}</span>
                     <span class="discount">(${item.discount}% oFF)</span>
                 </div>
-                <button class="btn-add-bag">Add To bag</button>
+                <button class="btn-add-bag" onclick="addToBag(${item.id})">Add To bag</button>
             </div>`
-});
+    });
 
-itemsContainerElement.innerHTML = innerHTML;
+
+    itemsContainerElement.innerHTML = innerHTML;
+}
+
+
+
+
+
+
